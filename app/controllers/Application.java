4b754717dd8b40e90a766fe3c5a88ff1e0d42e8d
Andrey Zelyaev <zellapresident@gmail.com>
2015-01-12 07:20:39 +0600

    ajax call now work

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,6 +5,7 @@ import com.feth.play.module.pa.user.AuthUser;
 import model.Comment;
 import model.Post;
 import model.User;
+import play.Routes;
 import play.data.Form;
 import play.mvc.*;
 
@@ -24,29 +25,40 @@ public class Application extends Controller {
         return redirect(routes.Application.index());
     }
 
-//    public static User getLocalUser(final Session session) {
-//        final User localUser = User.findByAuthUserIdentity(PlayAuthenticate
-//                .getUser(session));
-//        return localUser;
-//    }
-
     public static Result index() {
         return ok(index.render(Post.findAllPosts()));
     }
 
     public static Result detailPost(String postId) {
-        return ok(detailpost.render(Post.findById(postId)));
+        //TODO implicit user passing
+        return ok(detailpost.render(Post.findById(postId), getLocalUser(session())));
     }
 
     @Security.Authenticated(Secured.class)
     public static Result addComment(String postId) {
         Form<Comment> commentForm = form(Comment.class).bindFromRequest();
         Comment comment = commentForm.get();
+        comment.setName(getLocalUser(session()).getName());
         Post post = Post.findById(postId);
         post.addComment(comment);
         post.save();
-        return ok();
+        return ok("Ajax call!!!");
     }
+
+    public static Result javascriptRoutes() {
+        response().setContentType("text/javascript");
+        return ok(
+                Routes.javascriptRouter("jsRoutes",
+                        controllers.routes.javascript.Application.ajaxCall()
+                )
+        );
+    }
+
+    public static Result ajaxCall( ) {
+        System.out.println("ajax");
+        return ok("Ajax call!!!");
+    }
+
 
     public static User getLocalUser(final Http.Session session) {
         final AuthUser identity = PlayAuthenticate.getUser(session);
--- public/javascripts/hello.js
+++ public/javascripts/hello.js
@@ -1,3 +1,37 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
+
+
+
+var func = function () {
+
+    alert('cl');
+    jsRoutes.controllers.Application.ajaxCall().ajax({
+        success: function (data) {
+            alert("ajax!")
+        },
+        error: function () {
+            alert("Error!")
+        }
+    })
+};
+
+window.onload = func
+
+var ajaxCall = function () {
+
+    alert('mama');
+    var ajaxCallBack = {
+        success: onSuccess,
+        error: onError
+    }
+
+    jsRoutes.controllers.Application.ajaxCall().ajax(ajaxCallBack);
+
+};
+
+var onSuccess = function (data) {
+    alert(data);
+}
+
+var onError = function (error) {
+    alert(error);
 }
