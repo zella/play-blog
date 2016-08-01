9b9e5321d9776d47ebeaed6033cf3ab033cf12f2
Zella <zellapresident@gmail.com>
2016-08-01 16:23:30 +0600

    fix #37

--- app/utils/TextUtils.java
+++ app/utils/TextUtils.java
@@ -21,132 +21,52 @@ import java.util.regex.Pattern;
  */
 public class TextUtils {
 
-   public static final int TRUNCATED_CHAR_COUNT = 250;
-   public static final int TRUNCATED_WORD_COUNT = 150;
-   public static final int TRUNCATED_LINE_COUNT = 20;
+   public static final int TRUNCATED_CHAR_COUNT = 500;
+   public static final int TRUNCATED_LINE_COUNT = 10;
 
-   /**
-    * Copyright (c) Django Software Foundation and individual contributors.
-    * All rights reserved.
-    * <p>
-    * Copyright (c) 2011 Masood Behabadi <masood@dentcat.com>
-    * <p>
-    * Redistribution and use in source and binary forms, with or without modification,
-    * are permitted provided that the following conditions are met:
-    * <p>
-    * 1. Redistributions of source code must retain the above copyright notice,
-    * this list of conditions and the following disclaimer.
-    * <p>
-    * 2. Redistributions in binary form must reproduce the above copyright
-    * notice, this list of conditions and the following disclaimer in the
-    * documentation and/or other materials provided with the distribution.
-    * <p>
-    * 3. Neither the name of Django nor the names of its contributors may be used
-    * to endorse or promote products derived from this software without
-    * specific prior written permission.
-    * <p>
-    * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-    * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-    * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-    * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-    * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-    * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-    * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-    * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-    */
+   private final static String NON_THIN = "[^iIl1\\.,']";
 
-   @Deprecated
-   public static String truncateHtmlWords(String html, int length) {
-      if (length <= 0)
-         return new String();
-
-      List<String> html4Singlets = Arrays.asList(
-          "br", "col", "link", "base", "img",
-          "param", "area", "hr", "input");
-      // Set up regular expressions
-      Pattern pWords = Pattern.compile("&.*?;|<.*?>|(\\w[\\w-]*)");
-      Pattern pTag = Pattern.compile("<(/)?([^ ]+?)(?: (/)| .*?)?>");
-      Matcher mWords = pWords.matcher(html);
-      // Count non-HTML words and keep note of open tags
-      int endTextPos = 0;
-      int words = 0;
-      List<String> openTags = new ArrayList<String>();
-      while (words <= length) {
-         if (!mWords.find())
-            break;
-         if (mWords.group(1) != null) {
-            // It's an actual non-HTML word
-            words += 1;
-            if (words == length)
-               endTextPos = mWords.end();
-            continue;
-         }
-         // Check for tag
-         Matcher tag = pTag.matcher(mWords.group());
-         if (!tag.find() || endTextPos != 0)
-            // Don't worry about non tags or tags after our
-            // truncate point
-            continue;
-         String closingTag = tag.group(1);
-         // Element names are always case-insensitive
-         String tagName = tag.group(2).toLowerCase();
-         String selfClosing = tag.group(3);
-         if (closingTag != null) {
-            int i = openTags.indexOf(tagName);
-            if (i != -1)
-               openTags = openTags.subList(i + 1, openTags.size());
-         } else if (selfClosing == null && !html4Singlets.contains(tagName))
-            openTags.add(0, tagName);
-      }
-
-      if (words <= length)
-         return html;
-      StringBuilder out = new StringBuilder(html.substring(0, endTextPos));
-      for (String tag : openTags)
-         out.append("");
-
-      return out.toString();
+   private static int textWidth(String str) {
+      return (int) (str.length() - str.replaceAll(NON_THIN, "").length() / 2);
    }
 
-   @Deprecated
-   public static String truncateStringLineBreak(String what, int numberChars) {
-      if (what.length() <= numberChars) return what;
-      BreakIterator bi = BreakIterator.getLineInstance();
-      bi.setText(what);
-      int first_after = bi.following(numberChars);
-      what = what.substring(0, first_after);
-      return what;
+
+   public static String ellipsize(String text, int maxChar) {
+
+      //TODO line count
+
+      if (textWidth(text) <= maxChar)
+         return text;
+
+      // Start by chopping off at the word before max
+      // This is an over-approximation due to thin-characters...
+      int end = text.lastIndexOf(' ', maxChar - 3);
+
+      // Just one long word. Chop it off.
+      if (end == -1)
+         return text.substring(0, maxChar-3) + "...";
+
+      // Step forward as long as textWidth allows.
+      int newEnd = end;
+      do {
+         end = newEnd;
+         newEnd = text.indexOf(' ', end + 1);
+
+         // No more spaces.
+         if (newEnd == -1)
+            newEnd = text.length();
+
+      } while (textWidth(text.substring(0, newEnd) + "...") < maxChar);
+
+      return text.substring(0, end) + "...";
    }
 
-   public static String truncateStringByWordCount(String what, int wordCount) {
-
-      if (what.length()<= wordCount) return what;
-
-      BreakIterator breakIterator = BreakIterator.getWordInstance();
-      breakIterator.setText(what);
-
-      int currentWord = 0;
-
-      int boundaryIndex = breakIterator.first();
-      while (boundaryIndex != BreakIterator.DONE && currentWord < wordCount) {
-         currentWord ++;
-         boundaryIndex = breakIterator.next();
-      }
-
-      return what.substring(0, boundaryIndex);
-   }
 
    //TODO if images.count>1 reduce it. use jsoup.
    //TODO not working properly
    public static String generateTruncatedHtmlPreview(String html) {
 
-      //Temporary. Need time to develop proper html truncator with jsoup
-      String truncatedHtml = TextUtils.truncateStringByWordCount(html, TRUNCATED_WORD_COUNT);
-
-      truncatedHtml += "<br/> . . . ";
-      return truncatedHtml;
+      return TextUtils.ellipsize(html, TRUNCATED_CHAR_COUNT);
    }
 
    /**
